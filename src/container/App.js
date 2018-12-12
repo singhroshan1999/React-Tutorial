0a31abffc9dacf29a123b4570794b15d880f52fe
Roshan Singh <singhroshan1999@gmail.com>
2018-12-12 12:32:42 +0530

    1

--- src/container/App.js
+++ src/container/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Cockpit from './cockpit/cockpit';
+import Persons from './persons/persons';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
 
+      <Cockpit />
+      <Persons />
        
       </div>
     );
